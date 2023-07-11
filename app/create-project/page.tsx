c67c1c1f5f883c3a237ded38cd4555819bd05c36
Avi <avibanerjee2000@gmail.com>
2023-07-12 01:25:43 +0530

    .

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,6 +1,19 @@
-const CreateProject = () => {
+import Modal from '@/components/Modal';
+import ProjectForm from '@/components/ProjectForm';
+import { getCurrentUser } from '@/lib/session';
+import { redirect } from 'next/navigation';
+
+
+const CreateProject = async() => {
+  const session = await getCurrentUser();
+
+  if(!session?.user) redirect('/') 
+
   return (
-    <div>createProject</div>
+    <Modal>
+      <h3 className="modal-head-text">New Post</h3>
+      <ProjectForm type="create" session={session} />
+    </Modal>
   )
 }
 
